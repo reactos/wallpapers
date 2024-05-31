fdbe677cc73f1685b6ea0fd413f221817ef67ffc
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-05-31 09:20:29 +0900

    [WALLPAPERS] Restructure and add build.sh script

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # wallpapers
 
 This repository contains wallpapers for use in rapps.
-
-- `data/` ― ReactOS Standard Wallpapers
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+OLD_DIR=`pwd`
+NOW_DIR=$(dirname "$0")
+
+echo NOW_DIR=$NOW_DIR
+cd $NOW_DIR
+pwd
+
+OUTPUT_DIR=`pwd`/output
+ZIP_FILE=$OUTPUT_DIR/wallpapers.zip
+
+echo OUTPUT_DIR=$OUTPUT_DIR
+echo ZIP_FILE=$ZIP_FILE
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+fi
+
+if [ -f "$ZIP_FILE" ]; then
+    rm "$ZIP_FILE"
+fi
+
+WALLPAPERS_LIST=`ls **{,/**}/*.{jpg,png}`
+
+zip -j "$ZIP_FILE" $WALLPAPERS_LIST
+
+cd $OLD_DIR
